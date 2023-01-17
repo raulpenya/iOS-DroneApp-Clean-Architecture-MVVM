72cfac331e6b540bdfba04ca1630d1e63de6cbe5
rpenya <rpenya.mail@gmail.com>
2023-01-17 19:59:14 +0100

    create projects and targets

---
+++ drone/drone/Data/Data.h
@@ -0,0 +1,18 @@
+//
+//  Data.h
+//  Data
+//
+//  Created by raulbot on 17/1/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Data.
+FOUNDATION_EXPORT double DataVersionNumber;
+
+//! Project version string for Data.
+FOUNDATION_EXPORT const unsigned char DataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Data/PublicHeader.h>
+
+
