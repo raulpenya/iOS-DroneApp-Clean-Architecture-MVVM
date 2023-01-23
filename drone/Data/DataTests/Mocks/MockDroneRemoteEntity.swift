2229958adae9dba6f92fc8e3819d2cc6c6531c3b
rpenya <rpenya.mail@gmail.com>
2023-01-23 17:06:40 +0100

    clean up code

--- drone/Data/DataTests/Mocks/MockDroneRemoteEntity.swift
+++ drone/Data/DataTests/Mocks/MockDroneRemoteEntity.swift
@@ -1,5 +1,5 @@
 //
-//  MocksDroneRemoteEntity.swift
+//  MockDroneRemoteEntity.swift
 //  DataTests
 //
 //  Created by raulbot on 23/1/23.
@@ -8,7 +8,7 @@
 import Foundation
 @testable import Data
 
-struct MocksDroneRemoteEntity {
+struct MockDroneRemoteEntity {
     static func getPosition() -> DroneRemoteEntity {
         return DroneRemoteEntity(currentPosition: MockPositionRemoteEntity.getPosition())
     }
