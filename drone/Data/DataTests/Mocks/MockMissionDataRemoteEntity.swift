8f330b85ca2a65f574bccb565d38c88d436e5dab
rpenya <rpenya.mail@gmail.com>
2023-01-23 11:20:34 +0100

    data tests wip

---
+++ drone/Data/DataTests/Mocks/MockMissionDataRemoteEntity.swift
@@ -0,0 +1,15 @@
+//
+//  MockMissionDataRemoteEntity.swift
+//  DataTests
+//
+//  Created by raulbot on 23/1/23.
+//
+
+import Foundation
+@testable import Data
+
+struct MockMissionDataRemoteEntity {
+    static func getMissionData() -> MissionDataRemoteEntity {
+        
+    }
+}
